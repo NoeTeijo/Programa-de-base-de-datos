fe8f2e2746701ce42d50d9e49c5ad53501fa61d7
Kain <Kain@Kain>
2023-12-14 18:28:17 +0100

    Version 1.5 PUBLIC CLASS SELLO SEPARATE AND JAVADOC UPDATE

--- GestionSellos/src/main/java/com/mycompany/gestionsellos/GestionSellos.java
+++ GestionSellos/src/main/java/com/mycompany/gestionsellos/GestionSellos.java
@@ -8,72 +8,10 @@ import java.util.Scanner;
  * Utiliza un menú interactivo.
  *
  * @author NTM
- * @version 1.4
+ * @version 1.5
  */
 public class GestionSellos {
    
-    /**
-     * Esta es la clase interna que representa un Sello con país, motivo y año de emisión.
-     */
-        class Sello {
-
-        private String pais;
-        private String motivo;
-        private int anoEmision;
-
-        /**
-         * Constructor para la clase Sello.
-         *
-         * @param pais         El país del sello.
-         * @param motivo       El motivo del sello.
-         * @param anoEmision   El año de emisión del sello.
-         */
-        public Sello(String pais, String motivo, int anoEmision) {
-            this.pais = pais;
-            this.motivo = motivo;
-            this.anoEmision = anoEmision;
-        }
-    
-        // Getters y Setters para los parámetros anteriores
-
-        /**
-         * Obtiene el país del sello.
-         *
-         * @return El país del sello.
-         */
-        public String getPais() {
-            return pais;
-        }
-
-        /**
-         * Obtiene el motivo del sello.
-         *
-         * @return El motivo del sello.
-         */
-        public String getMotivo() {
-            return motivo;
-        }
-
-        /**
-         * Obtiene el año de emisión del sello.
-         *
-         * @return El año de emisión del sello.
-         */
-        public int getAnoEmision() {
-            return anoEmision;
-        }
-
-        /**
-         * Convierte el sello en una cadena de texto.
-         *
-         * @return Representación en texto del sello.
-         */
-        @Override
-        public String toString() {
-            return "País: " + pais + ", Motivo: " + motivo + ", Año de emisión: " + anoEmision;
-        }
-    }
-
     // Establece un array de la clase Sello llamada sellos
 
     private ArrayList<Sello> sellos;
---
+++ GestionSellos/src/main/java/com/mycompany/gestionsellos/Sello.java
@@ -0,0 +1,76 @@
+package com.mycompany.gestionsellos;
+import java.util.ArrayList;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kain
+ */
+    /**
+     * Esta es la clase interna que representa un Sello con país, motivo y año de emisión.
+     */
+    public class Sello {
+
+        private String pais;
+        private String motivo;
+        private int anoEmision;
+
+        /**
+         * Constructor para la clase Sello.
+         *
+         * @param pais         El país del sello.
+         * @param motivo       El motivo del sello.
+         * @param anoEmision   El año de emisión del sello.
+         */
+        public Sello(String pais, String motivo, int anoEmision) {
+            this.pais = pais;
+            this.motivo = motivo;
+            this.anoEmision = anoEmision;
+        }
+    
+        // Getters y Setters para los parámetros anteriores
+
+        /**
+         * Obtiene el país del sello.
+         *
+         * @return El país del sello.
+         */
+        public String getPais() {
+            return pais;
+        }
+
+        /**
+         * Obtiene el motivo del sello.
+         *
+         * @return El motivo del sello.
+         */
+        public String getMotivo() {
+            return motivo;
+        }
+
+        /**
+         * Obtiene el año de emisión del sello.
+         *
+         * @return El año de emisión del sello.
+         */
+        public int getAnoEmision() {
+            return anoEmision;
+        }
+
+        /**
+         * Convierte el sello en una cadena de texto.
+         *
+         * @return Representación en texto del sello.
+         */
+        @Override
+        public String toString() {
+            return "País: " + pais + ", Motivo: " + motivo + ", Año de emisión: " + anoEmision;
+        }
+    }
+
+
+
+
