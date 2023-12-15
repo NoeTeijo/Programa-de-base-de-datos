78ccfd2042515662da460d2c0d8bfd01c031f9cf
Kain <Kain@Kain>
2023-12-15 17:10:33 +0100

    Version 1.5 TestAgregarMostrarSellos DONE

--- GestionSellos/src/main/java/com/mycompany/gestionsellos/Sello.java
+++ GestionSellos/src/main/java/com/mycompany/gestionsellos/Sello.java
@@ -1,5 +1,4 @@
 package com.mycompany.gestionsellos;
-import java.util.ArrayList;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
